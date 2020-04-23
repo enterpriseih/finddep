396b5a65009a45f9d6ae3666aa8f26a40f8b5883
wangchangxin1 <wangchangxin1@jd.com>
2020-04-23 17:10:17 +0800

    change absolute path

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // 暴露出查询依赖配置
 // rootPath 绝对路径
-export const rootPath = '/Users/wangchangxin1/project/wqshop/shop';
+export const rootPath = 'xxxxx';
 export const fileType = ['.js'];
 export const exclude = '';
